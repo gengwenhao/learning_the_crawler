2121721dccd6469555c32217ece2d83c7c1ecb2a
gengwenhao <gengwenhao97@126.com>
2018-08-10 17:27:51 +0800

    学习cookiejar和HTTPCookieProcessor的使用

--- learn_urllib/learn_auto_cookie.py
+++ learn_urllib/learn_auto_cookie.py
@@ -9,43 +9,96 @@ from urllib.parse import urlencode
 
 BASE_DIR = os.path.dirname(__file__)
 
-login_url = 'http://www.renren.com/ajaxLogin/login?1=1&uniqueTimestamp=201875142656'
-target_url = 'http://www.renren.com/880151247/profile/'
+# 登录地址
+LOGIN_URL = 'http://www.renren.com/ajaxLogin/login?1=1&uniqueTimestamp=201875142656'
 
-# 手动在请求头中添加cookie字段
-headers = {
-    'User-Agent': 'Mozilla/5.0',
-    'Referer': 'http://www.baidu.com',
-}
+# 要访问地址
+TARGET_URL = 'http://www.renren.com/880151247/profile/'
 
-# 创建cookiejar对象
-cookie_jar = CookieJar()
-
-# 使用coookiejar创建HTTPCookieProcess对象
-cookie_handler = request.HTTPCookieProcessor(cookie_jar)
-
-# 使用handler创建opener
-opener = request.build_opener(cookie_handler)
-
-# 发送登录请求
-login_data = dict(
+# 登录表单
+LOGIN_FORM_DATA = dict(
     autoLogin=True,
     captcha_type='web_login',
     domain='renren.com',
-    email='gengwenhao97@126.com',
+    email='',
     f='http%3A%2F%2Fwww.renren.com%2F396330976',
     icode='点尺无短',
     key_id=1,
     origURL='http://www.renren.com/home',
-    password='30746ed326031d02c4c3a98ce25d5900ce80ad4807b80919847eaa596eb973e7',
+    password='',
     rkey='e37cf7c04307ba10c1b50a9a887570ca',
 )
-encode_login_data = urlencode(login_data).encode('utf-8')
-req = request.Request(url=login_url, data=encode_login_data, headers=headers, method='POST')
-opener.open(req)
 
-# 访问目标站点
-resp = opener.open(target_url)
-with open('index.html', 'w', encoding='utf-8') as f:
-    f.write(resp.read().decode('utf-8'))
-    webbrowser.open(os.path.join(BASE_DIR, 'index.html'))
+# 通用请求头
+DEFAULT_HEADERS = {
+    'User-Agent': 'Mozilla/5.0',
+    'Referer': 'http://www.baidu.com',
+}
+
+
+def get_cookie_opener():
+    """
+        带cookie配置的opener
+    :return:
+    """
+    # 创建cookiejar对象
+    cookie_jar = CookieJar()
+
+    # 使用coookiejar创建HTTPCookieProcess对象
+    cookie_handler = request.HTTPCookieProcessor(cookie_jar)
+
+    # 使用handler创建opener
+    _opener = request.build_opener(cookie_handler)
+
+    return _opener
+
+
+def login_target_url(opener, form_data, login_url, headers):
+    """
+        登录保存cookie
+    :param opener:
+    :param form_data:
+    :param login_url:
+    :param headers:
+    :return:
+    """
+    # 先url编码，之后utf8编码
+    data_bytes = urlencode(form_data).encode('utf-8')
+
+    req = request.Request(
+        url=login_url,
+        data=data_bytes,
+        headers=headers,
+        method='POST'
+    )
+
+    _resp = opener.open(req)
+
+    return _resp
+
+
+def visit_profile_site(opener, target_url):
+    """
+        通过cookie访问目标站点
+    :param opener:
+    :param target_url:
+    :return:
+    """
+    # 访问目标站点
+    _resp = opener.open(target_url)
+
+    with open('index.html', 'w', encoding='utf-8') as f:
+        html_str = _resp.read().decode('utf-8')
+        f.write(html_str)
+        webbrowser.open(os.path.join(BASE_DIR, 'index.html'))
+
+    return _resp
+
+
+if __name__ == '__main__':
+    # 网络请求函数 opener
+    opener_with_cookie = get_cookie_opener()
+    # 登录
+    login_target_url(opener_with_cookie, LOGIN_FORM_DATA, LOGIN_URL, DEFAULT_HEADERS)
+    # 访问
+    visit_profile_site(opener_with_cookie, TARGET_URL)
